5428fda94691aab6bd2091e2c67e226d48d48607
ludmilaalima <ludmilalima15z@gmail.com>
2025-09-30 23:22:34 -0300

    feat: add voo card

--- create_update_csv.py
+++ create_update_csv.py
@@ -13,7 +13,7 @@ class CreateUpdateCsv:
     def create_csv(self):
         if "data.csv" not in os.listdir(Path.cwd()):
     
-            df = pd.DataFrame(columns=['id', 'price', "date"])
+            df = pd.DataFrame(columns=['id', 'departure_time_local', 'arrival_time_local', 'airline_name', 'duration_iso8601', 'destination_iata', 'stop_count', 'origin_iata' 'emissions_co2e_kg', 'price', "extracted_at"])
             df.to_csv("data.csv", index=False)
            
 
@@ -28,7 +28,7 @@ class CreateUpdateCsv:
             'date' : date
         }
 
-        pd.DataFrame([new_line], columns = ['id', 'price', 'date']).to_csv(
+        pd.DataFrame([new_line], columns = ['id', 'departure_time_local', 'arrival_time_local', 'airline_name', 'duration_iso8601', 'destination_iata', 'stop_count', 'origin_iata' 'emissions_co2e_kg', 'price', "extracted_at"]).to_csv(
             self.data_link, mode='a', header=False, index=False
         )
         
