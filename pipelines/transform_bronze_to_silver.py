91a6396b181815e94bac1d1482fbfc71e3cbbd6c
ludmilaalima <ludmilalima15z@gmail.com>
2026-01-09 23:49:52 -0300

    refactor: incomplete restructure

--- pipelines/transform_bronze_to_silver.py
+++ pipelines/transform_bronze_to_silver.py
@@ -1,2 +1,13 @@
 # bronze > silver
+import json
 
+
+
+class BronzeToSilver:
+    def __init__(self):
+        self.bronze_file = r"data/bronze/bronze_flights_raw.jsonl"
+
+    
+
+    def load_bronzer(self):
+        data = json.loads(self.bronze_file)
