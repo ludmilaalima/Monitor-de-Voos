77ec0a9ed2534be4631ff3a08a5ebceff326d6ec
ludmilaalima <ludmilalima15z@gmail.com>
2026-01-13 22:52:01 -0300

    refactor: functions finished

--- pipelines/storage.py
+++ pipelines/storage.py
@@ -7,12 +7,12 @@ import shortuuid
 import json
 
 
-KNOW_AIRLINES = ['AZUL', 'LATAM', 'GOL']
+
 
 class CreateUpdateFiles:
     def __init__(self):
         # bronze - raw
-        data_bronze_raw =  'bronze_flights_raw.jsonl'
+        data_bronze_raw =  Path("data/bronze/bronze_flights_raw.jsonl") 
         self.data_bronze_raw = Path(data_bronze_raw)
 
         # silver 
@@ -31,7 +31,8 @@ class CreateUpdateFiles:
             df = pd.DataFrame(columns=['id', 'origin_iata', 'destination_iata', 'departure_time_local', 'arrival_time_local', 'main_airline', 'all_airlines', 'duration_iso8601', 'duration_minutes', 'num_stops', 'emissions_raw', 'emissions_co2e_kg', 'price', "extracted_at"])
             df.to_csv(self.data_silver, index=False, encoding='utf-8-sig')
 
-    def save_bronze(self, text):
+
+    def update_bronze(self, text):
 
         date = datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
         id = shortuuid.uuid()[:8]
@@ -43,136 +44,6 @@ class CreateUpdateFiles:
 
         return id, date
     
-
-    def find_h(text):
-        position_h = 0
-
-        for i in range(3, len(text)):
-            extract = text[i]
-            find_h = re.search(r'\b\d+h\b', extract)
-
-            if find_h:
-                break
-            else:
-                position_h += 1
-
-        return position_h
-
-
-    def find_price(text):
-        for i, string in enumerate(text):
-            if 'R$' in string:
-                return i, text[i+1]
-        return None, None
-        
-
-    def find_kg(text, price_index):
-        for i, string in enumerate(text):
-            if 'kg' in string:
-                return text[i-1:price_index]
-        
-        return None, None
-            
-    def find_iatas(text):
-        for string in text:
-            match = re.search(r'\b([A-Z]{3})\s*[-–—]\s*([A-Z]{3})\b', string)
-            if match:
-                origin_iata = match.group(1)
-                destination_iata = match.group(2)
-                return origin_iata, destination_iata
-        
-
-        return None, None
-    
-    def extract_airlines(raw_airline, know_airlines=KNOW_AIRLINES):
-        if raw_airline is None:
-            return None, None
-
-        if isinstance(raw_airline, list):
-            raw_airline = " ".join(raw_airline)
-
-        raw_airline = raw_airline.upper()
-
-
-        found = []
-        for airline in know_airlines:
-            if airline in raw_airline:
-                found.append(airline) 
-
-        raw_airline = raw_airline.split()
-        all_airlines = []
-        for string in raw_airline: # GOL, LATAN, AZUL
-            for know_airline in found: # AZUL, LATAM, GOL
-                if know_airline in string:
-                    if know_airline in all_airlines:
-                        continue
-                    all_airlines.append(know_airline)
-
-        main_airline = all_airlines[0]
-        all_airlines = " | ".join(all_airlines)
-
-        return main_airline, all_airlines
-
-            
-    def extract_card(self, text, position_h, price, kg_index_raw, origin, destination, id, extracted_at):
-        
-        kwargs = {}
-        all_airline_name = ''
-        
-        airline_name_start = 3
-        airline_name_end = airline_name_start + position_h 
-        # inicio e fim da string 
-        if position_h > 1:
-            all_airline_name = text[airline_name_start:airline_name_end]
-        else:
-            all_airline_name = text[3]
-
-        # quantas paradas 
-        escale = ''
-        for i in range(0, len(text)): 
-            if text[i] == 'Sem':
-                escale = 0
-            elif text[i] in ('parada', 'paradas'):
-                escale = text[i-1]
-
-
-        # aeroporto inicial e final
-        raw_airline = all_airline_name
-        main_airline, all_airlines = self.extract_airlines(raw_airline)
-
-
-        #duration iso8601
-        hour = text[airline_name_end]
-        minute = text[airline_name_end+1]
-
-        if len(minute) > 3:
-            minute = 00
-
-        print(f"{hour} {minute}")
-
-        # consertar horas 
-        hour = hour.replace("h", "")
-
-        kwargs["id"] = id
-        kwargs['extracted_at'] = extracted_at
-        kwargs["main_airline"] = main_airline
-        kwargs["all_airlines"] = all_airlines
-        kwargs["price"] = float(price)
-        kwargs["kg_index_raw"] = kg_index_raw
-        kwargs["origin_iata"] = origin
-        kwargs["destination_iata"] = destination
-        kwargs["duration_iso8601"] = f"PT{hour}H{minute}M"
-        kwargs["duration_minutes"] = int(hour) * 60 + int(minute)
-
-        
-        # campos obrigatorios
-        check_all = [id, origin, destination, price, text[0], text[2]]
-        all_not_null = all(v is not None for v in check_all)
-
-        if all_not_null:
-            self.update_silver(text, escale, **kwargs)
-
-
 
     def update_silver(self, text, escale, id, extracted_at, main_airline, all_airlines, price, kg_index_raw, origin_iata, destination_iata, duration_iso8601, duration_minutes):
 
--- pipelines/transform_bronze_to_silver.py
+++ pipelines/transform_bronze_to_silver.py
@@ -3,17 +3,19 @@ import json
 from pipelines.storage import CreateUpdateFiles
 import re
 from pathlib import Path
+from parsers.google_flights_parser import GoogleFlightsParser
 
 
 
 class BronzeToSilver:
     def __init__(self):
-        self.bronze_file = Path("data/bronze/bronze_flights_raw.jsonl")
-        self.create_update_files = CreateUpdateFiles() 
+        self.bronze_file = Path("data/bronze/bronze_flights_raw.jsonl") 
+        self.parser = GoogleFlightsParser()
+        self.storage = CreateUpdateFiles()
         
     
 
-    def load_bronzer(self):
+    def load_bronze(self):
         lines_raw = []
 
         with self.bronze_file.open('r', encoding='utf-8') as f:
@@ -22,13 +24,14 @@ class BronzeToSilver:
                 lines_raw.append(line_raw)
     
         #### refatorar logica de list e dict
-        for line in lines_raw['text']:
-            text = re.sub(r"\s+", " ", text) 
+        for item in lines_raw:
+            text = re.sub(r"\s+", " ", item['text']) 
             text = text.split()
-            position_h = self.create_update_files.find_h(text)
-            index_price, price = self.create_update_files.find_price(text)
-            kg_index_raw = self.create_update_files.find_kg(text, index_price)
-            origin, destination = self.create_update_files.find_iatas(text)
+            position_h = self.parser.find_h(text)
+            index_price, price = self.parser.find_price(text)
+            kg_index_raw = self.parser.find_kg(text, index_price)
+            origin, destination = self.parser.find_iatas(text)
             print(text)
-            self.create_update_files.extract_card(text, position_h, price, kg_index_raw, origin, destination, line, line['extracted_at'])
-
+            parsed = self.parser.parse_card(text, position_h, price, kg_index_raw, origin, destination, item["id"], item['extracted_at'])
+            if parsed:
+                self.storage.update_silver(parsed)
