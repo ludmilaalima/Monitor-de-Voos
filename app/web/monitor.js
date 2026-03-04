9246d79fe56ff9cab31e911f8a81dbe8d1959bbb
ludmilaalima <ludmilalima15z@gmail.com>
2026-03-03 21:58:33 -0300

    wip: add js

--- app/web/monitor.js
+++ app/web/monitor.js
@@ -1,12 +1,21 @@
-document.getElementById("monitorForm").addEventListener('submit', async (e) => {
+const form = document.getElementById('monitorForm');
+form.addEventListener('submit', async(e) => {
     e.preventDefault();
- })
+})
+
+const formData = new FormData(e.target);
+const data = Object.fromEntries(formData.entries());
 
 
- const data = Object.fromEntries(new FormData(e.target).entries());
+try{
+    const response = await fetch('/criar-monitor', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json,'
 
+        }
+    })} 
+    finally{
 
- 
-
-
+    }
 
