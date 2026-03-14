a0c8c249a9214438bae1882dd4161747a154eea2
ludmilaalima <ludmilalima15z@gmail.com>
2026-03-13 21:56:04 -0300

    wip: create a flights page

--- parsers/pages/google_flights_page.py
+++ parsers/pages/google_flights_page.py
@@ -2,7 +2,7 @@
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
-driver.get("https://www.google.com/travel/flights/search?tfs=CBwQAhoeEgoyMDI2LTA0LTAyagcIARIDQ1BWcgcIARIDQ05GQAFIAXABggELCP___________wGYAQI&tfu=EgoIABABGAAgAigK")
+driver.get("")
 
 campo_origem = driver.find_element(
     By.CSS_SELECTOR,
@@ -11,41 +11,45 @@ campo_origem = driver.find_element(
 
 print(campo_origem)'''
 
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-options = webdriver.ChromeOptions()
-options.add_argument("--lang=pt-BR")
 
-driver = webdriver.Chrome(options=options)
-wait = WebDriverWait(driver, 20)
 
-driver.get("https://www.google.com/travel/flights/search?tfs=CBwQAhoeEgoyMDI2LTA0LTAyagcIARIDQ1BWcgcIARIDQ05GQAFIAXABggELCP___________wGYAQI&tfu=EgoIABABGAAgAigK")
+driver = webdriver.Chrome()
+wait = WebDriverWait(driver, 5)
 
-# espera o carregamento geral
-wait.until(lambda d: d.execute_script("return document.readyState") == "complete")
 
-# tenta fechar consentimento/cookies, se aparecer
-for xpath_botao in [
-    "//button[normalize-space()='Aceitar tudo']",
-    "//button[normalize-space()='Accept all']",
-]:
-    try:
-        wait.until(EC.element_to_be_clickable((By.XPATH, xpath_botao))).click()
-        break
-    except:
-        pass
 
-# tenta localizar em português ou inglês
-campo_origem = wait.until(
+driver.get('https://www.google.com/travel/flights/search?tfs=CBwQAhoeEgoyMDI2LTA0LTAyagcIARIDME5OcgcIARIDMFREQAFIAXABggELCP___________wGYAQI&tfu=EgoIABABGAAgAigD&hl=pt-BR&gl=BR')
+
+origin = driver.find_element(By.CSS_SELECTOR, 'input[aria-label*="De onde?"]')
+origin.send_keys(Keys.CONTROL, 'a')
+origin.send_keys(Keys.DELETE)
+origin.send_keys("CNF")
+
+
+select_iata_aeroport = wait.until(EC.element_to_be_clickable((By.XPATH, "//ul[@role='listbox']//li[@role='option'][contains(., 'Aeroporto')]")))
+select_iata_aeroport.click()
+time.sleep(10)
+
+
+'''wait.until(
     EC.element_to_be_clickable((
         By.XPATH,
-        "//*[@role='textbox' and (@data-placeholder='De onde?' or @data-placeholder='Where from?')]"
+        "//ul[@role='listbox']//li[@role='option'][contains(., 'Aeroporto')]"
     ))
 )
+primeira_opcao.click()
 
-print("Elemento encontrado:", campo_origem.text)
-campo_origem.click()
+
+time.sleep(10)'''
+
+
+
+
