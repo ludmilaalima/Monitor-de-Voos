e50e82cca15e7601a09709473b76533e841ee5b9
ludmilaalima <ludmilalima15z@gmail.com>
2026-03-14 19:54:56 -0300

    wip: flights page -> start

--- parsers/pages/google_flights_page.py
+++ parsers/pages/google_flights_page.py
@@ -1,16 +1,3 @@
-'''from selenium import webdriver
-from selenium.webdriver.common.by import By
-
-driver = webdriver.Chrome()
-driver.get("")
-
-campo_origem = driver.find_element(
-    By.CSS_SELECTOR,
-    'div[role="textbox"][data-placeholder="De onde?"]'
-)
-
-print(campo_origem)'''
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -22,7 +9,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 driver = webdriver.Chrome()
-wait = WebDriverWait(driver, 5)
+wait = WebDriverWait(driver, 10)
 
 
 
@@ -31,25 +18,23 @@ driver.get('https://www.google.com/travel/flights/search?tfs=CBwQAhoeEgoyMDI2LTA
 origin = driver.find_element(By.CSS_SELECTOR, 'input[aria-label*="De onde?"]')
 origin.send_keys(Keys.CONTROL, 'a')
 origin.send_keys(Keys.DELETE)
-origin.send_keys("CNF")
+origin.send_keys("CPV")
 
 
-select_iata_aeroport = wait.until(EC.element_to_be_clickable((By.XPATH, "//ul[@role='listbox']//li[@role='option'][contains(., 'Aeroporto')]")))
-select_iata_aeroport.click()
+select_iata_origin = wait.until(EC.element_to_be_clickable((By.XPATH, "//ul[@role='listbox']//li[@role='option'][contains(., 'Aeroporto')]")))
+select_iata_origin.click()
+
+
+
+destination = driver.find_element(By.CSS_SELECTOR, 'input[aria-label*="Para onde?"]')
+destination.send_keys(Keys.CONTROL, 'a')
+destination.send_keys(Keys.DELETE)
+destination.send_keys('CNF')
+
+select_iata_destination = wait.until(EC.element_to_be_clickable((By.XPATH, "//ul[@role='listbox']//li[@role='option'][contains(., 'Aeroporto')]")))
+select_iata_destination.click()
+
+
+
+
 time.sleep(10)
-
-
-'''wait.until(
-    EC.element_to_be_clickable((
-        By.XPATH,
-        "//ul[@role='listbox']//li[@role='option'][contains(., 'Aeroporto')]"
-    ))
-)
-primeira_opcao.click()
-
-
-time.sleep(10)'''
-
-
-
-
